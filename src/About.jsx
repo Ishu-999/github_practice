d59800fbd95e553d28944feb453348827b4c0d6d
Ishu developer <ishudeveloper@gmail.com>
2024-05-01 12:23:29 +0530

    about

---
+++ src/About.jsx
@@ -0,0 +1,7 @@
+export const About =()=>{
+    return (
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur amet alias at consequuntur provident similique, nemo natus unde perspiciatis fugit iure nesciunt accusantium itaque, doloremque cupiditate veniam quas adipisci iusto!
+        </div>
+    )
+}
